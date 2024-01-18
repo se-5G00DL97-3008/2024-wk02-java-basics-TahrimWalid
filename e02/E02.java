bd55c0fb9cff65dfdcb19b0a31cc21d44d83569f
Walid Hassan Tahrim <tahrimwalid@gmail.com>
2024-01-18 02:22:25 +0200

    modified codes 1.0

--- e02/E02.java
+++ e02/E02.java
@@ -7,7 +7,7 @@ class E02 {
         System.out.println("Give a number:");
         int num1 = scanner.nextInt();
 
-        System.out.println("Give another number:");
+        System.out.println("Give a number:");
         int num2 = scanner.nextInt();
 
         System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
