20e03a0ab28781f8d08fd3d5bd4130102e8037b4
Walid Hassan Tahrim <tahrimwalid@gmail.com>
2024-01-18 02:46:33 +0200

    modified codes 7.0

--- e02/E02.java
+++ e02/E02.java
@@ -14,13 +14,7 @@ class E02 {
         System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
         System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
         System.out.println(num1 + " x " + num2 + " = " + (num1 * num2));
-
-        // Validation for division by 0 which is my personal touch
-        if (num2 != 0) {
-            System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
-        } else {
-            System.out.println("Cannot divide by zero.");
-        }
+        System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
 
         scanner.close();
     }
