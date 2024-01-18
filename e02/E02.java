99fa71c399c146f8ed8f6046c3aba294b034e94a
Walid Hassan Tahrim <tahrimwalid@gmail.com>
2024-01-18 02:56:43 +0200

    modified codes 7.0

--- e02/E02.java
+++ e02/E02.java
@@ -4,13 +4,12 @@ class E02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Give a number:");
+        System.out.println("Give a number: ");
         int num1 = scanner.nextInt();
 
-        System.out.println("Give a number:");
+        System.out.println("Give a number: ");
         int num2 = scanner.nextInt();
 
-        System.out.println();
         System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
         System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
         System.out.println(num1 + " x " + num2 + " = " + (num1 * num2));
--- e04/E04.java
+++ e04/E04.java
@@ -5,11 +5,11 @@ class E04 {
         Scanner scanner = new Scanner(System.in);
 
         // Ask the user to enter a day
-        System.out.println("Enter a day:");
+        System.out.println("Enter a day: ");
         int day = scanner.nextInt();
 
         // Ask the user to enter a month
-        System.out.println("Enter a month:");
+        System.out.println("Enter a month: ");
         int month = scanner.nextInt();
 
         if (day == 24 && month == 12) {
