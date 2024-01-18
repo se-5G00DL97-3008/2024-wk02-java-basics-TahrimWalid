6ceb3c5e646b20b8662827b753ac253ae3f3cd70
Walid Hassan Tahrim <tahrimwalid@gmail.com>
2024-01-18 02:58:54 +0200

    modified codes 7.0

--- e03/E03.java
+++ e03/E03.java
@@ -4,7 +4,7 @@ class E03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter a number:");
+        System.out.println("Enter a number: ");
         double number = scanner.nextDouble();
         System.out.println();
 
